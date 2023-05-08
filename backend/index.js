db153969c89a25db6e5e995ea89d67893808680f
daan98 <daan98.da@gmail.com>
2023-05-07 23:03:17 -0600

    handle domains that access to api

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import veterinarianRoutes from './routes/veterinarian.route.js';
 import patientRoutes from './routes/patient.route.js';
+import cors from 'cors';
 
 const app = express();
 
@@ -10,6 +11,18 @@ app.use(express.json())
 dotenv.config();
 
 connectDB();
+
+const acceptedDomains = ['http://localhost:3000', 'https://localhost:3000'];
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (acceptedDomains.indexOf(origin) !== -1) {
+            // request origin is accepted
+            callback(null, true);
+        } else {
+            callback( new Error("You have no access due to CORS.") );
+        }
+    },
+}
 
 app.use('/api/veterinarian', veterinarianRoutes);
 app.use('/api/patient', patientRoutes);
